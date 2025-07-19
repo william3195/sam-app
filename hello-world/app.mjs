7692d99661c70a20640d2b8ba1abdc5b95c43085
william3195 <william_canton@msn.com>
2025-07-19 17:07:36 -0400

    Remove simulated failures

--- hello-world/app.mjs
+++ hello-world/app.mjs
@@ -12,18 +12,18 @@
  */
 
 export const lambdaHandler = async (event, context) => {
-  throw new Error("This will cause a deployment rollback")
-  // try {
-  //     response = {
-  //         "statusCode": 200,
-  //         "body": JSON.stringify({
-  //             message: "I'm using canary deployments",
-  //         })
-  //     }
-  // } catch (err) {
-  //     console.log(err);
-  //     return err;
-  // }
+//  throw new Error("This will cause a deployment rollback")
+  try {
+      response = {
+          "statusCode": 200,
+          "body": JSON.stringify({
+              message: "I'm using canary deployments",
+          })
+      }
+  } catch (err) {
+      console.log(err);
+      return err;
+  }
 
-  // return response
+  return response
 };

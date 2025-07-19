adb9327eb64f165ce9436f63de8d47b0611f3818
william3195 <william_canton@msn.com>
2025-07-19 17:11:25 -0400

    Revert lambda  to original

--- hello-world/app.mjs
+++ hello-world/app.mjs
@@ -13,17 +13,15 @@
 
 export const lambdaHandler = async (event, context) => {
 //  throw new Error("This will cause a deployment rollback")
-  try {
-      response = {
-          "statusCode": 200,
-          "body": JSON.stringify({
-              message: "I'm using canary deployments",
-          })
-      }
-  } catch (err) {
-      console.log(err);
-      return err;
-  }
-
-  return response
+    try {
+        return {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: 'I\'m using canary deployments',
+            })
+       }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
 };
